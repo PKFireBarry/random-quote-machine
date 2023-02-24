292cf61036031ebe2f5d067e45b2bea23e87e05b
PKFireBarry <Barry0719@gmail.com>
2023-02-24 16:04:50 -0500

    Version 0.0.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  const fetchQuote = async () => {
+    const response = await fetch('https://api.quotable.io/random');
+    const data = await response.json();
+    setQuote(data.content);
+    setAuthor(data.author);
+  };
+
+  const handleClick = () => {
+    fetchQuote();
+  };
+
+  const tweetQuote = () => {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(`${quote} - ${author}`)}`;
+    window.open(twitterUrl, '_blank');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center">
+      <div id="quote-box">
+        <div className="quote-text">
+          <i className="fas fa-quote-left"></i>
+          <span id="text">{quote}</span>
+        </div>
+        <div className="quote-author">
+          <span id="author">- {author}</span>
+        </div>
+        <div className="buttons">
+          <button id="new-quote" onClick={handleClick}>New Quote</button>
+          <a id="tweet-quote" href="twitter.com/intent/tweet" onClick={tweetQuote}>Tweet Quote</a>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
